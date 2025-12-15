11e73838bfa6534983d8bb827816ecf6800fa675
viaarnas <viaardnas@gmail.com>
2025-12-15 10:09:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const modalBtn = document.getElementById("cara");
-    const showModal = document.getElementById("modal");
-    modalBtn.addEventListener("click", function(e) {
-      e.preventDefault();
-      showModal.style.display = "flex";
-    });
+const showModal = document.getElementById("modal");
+modalBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    showModal.style.display = "flex";
+});
     
-    const closeBtn = document.getElementById("close-btn");
-    closeBtn.addEventListener("click", function(e) {
-      e.preventDefault();
-      showModal.style.display = "none";
-    });
+const closeBtn = document.getElementById("close-btn");
+closeBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    showModal.style.display = "none";
+});
     
-    const showPic = document.getElementById("gambar");
-    showPic.addEventListener("click", function(e) {
-      e.preventDefault();
-      showModal.style.display = "flex";
-    });
+const showPic = document.getElementById("gambar");
+showPic.addEventListener("click", function(e) {
+    e.preventDefault();
+    showModal.style.display = "flex";
+});
+
+showModal.addEventListener("click", function(e) {
+    if (e.target === showModal) {
+    showModal.style.display = "none";
+    }
+});
+    
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") {
+    showModal.style.display = "none";
+    }
+});
